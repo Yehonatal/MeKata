044af04988ee7a40591dde8fc22e60bdc0e73276
Yonatan Afewerk Teshome <yonatanafewerk@gmail.com>
2023-03-07 12:50:35 +0300

    March 7 start of Big thing #1

---
+++ dist/apps/js/main.js
@@ -0,0 +1,41 @@
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+
+// Fillers
+const name = document.getElementById("name");
+const honor = document.getElementById("honor");
+const kata = document.getElementById("kata");
+const rank = document.getElementById("rank");
+const score = document.getElementById("score");
+
+addEventListener("load", () => {
+    fetch("https://www.codewars.com/api/v1/users/R4nn3r")
+        .then((response) => response.json())
+        .then((data) => {
+            // Use the data to update the content of your webpage
+            name.innerText = data.name;
+            honor.innerText = data.honor;
+            kata.innerText = data.codeChallenges.totalCompleted;
+            rank.innerText = data.ranks.overall.name;
+            score.innerText = data.ranks.overall.score;
+        })
+        .catch((error) => console.log(error));
+});
+
+button.addEventListener("click", (e) => {
+    const userName = input.value.trim();
+    if (userName.length == 0) return input.classList.add("error");
+
+    input.classList.remove("error");
+    fetch(`https://www.codewars.com/api/v1/users/${userName}`)
+        .then((response) => response.json())
+        .then((data) => {
+            // Use the data to update the content of your webpage
+            name.innerText = data.name;
+            honor.innerText = data.honor;
+            kata.innerText = data.codeChallenges.totalCompleted;
+            rank.innerText = data.ranks.overall.name;
+            score.innerText = data.ranks.overall.score;
+        })
+        .catch((error) => console.log(error));
+});
